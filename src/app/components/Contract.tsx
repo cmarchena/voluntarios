011eeaea9dccc8d815ad69b06a939fff8047b1ec
Carlos Marchena <sprintwithcarlos@pm.me>
2024-07-17 17:41:18 +0200

    Update Contract.tsx

--- src/app/components/Contract.tsx
+++ src/app/components/Contract.tsx
@@ -132,7 +132,7 @@ const Contract: React.FC = () => {
     <main>
       <Image
         alt="logo"
-        src={"/logo.png"}
+        src={"/voluntarios/logo.png"}
         width={400}
         height={100}
         className="logo"
